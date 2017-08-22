e4faa5c4be1f17d28caf60e4a9d0008c7b35963f
Valentin Wong <valentin.wong@gmail.com>
2017-08-22 09:21:33 -0700

    created schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -11,17 +11,17 @@ CREATE TABLE users
 
 CREATE TABLE bootcamps
 (
-	id int NOT NULL AUTO_INCREMENT,
-	bootcamp_name varchar(255) NOT NULL,
-	outcomes int NOT NULL,
-	location varchar(255) NOT NULL,
-  	logo varchar(255) NOT NULL,
-  	rating int NOT NULL,
-  	technology varchar(255) NOT NULL,
- 	duration int NOT NULL,
-  	comments varchar(255) NOT NULL,
-  	costs int NOT NULL,
-  	url varchar(255) NOT NULL
+		id int NOT NULL AUTO_INCREMENT,
+		bootcamp_name varchar(255) NOT NULL,
+		outcomes int NOT NULL,
+		location varchar(255) NOT NULL,
+	  	logo varchar(255) NOT NULL,
+	  	rating int NOT NULL,
+	  	technology varchar(255) NOT NULL,
+	 	duration int NOT NULL,
+	  	comments varchar(255) NOT NULL,
+	  	costs int NOT NULL,
+	  	url varchar(255) NOT NULL
 );
 
 CREATE TABLE comments
